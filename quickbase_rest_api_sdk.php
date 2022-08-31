3b4b905984665ff0e4fee2c2a34016fa73ab80db
Joseph Harburg <44932091+josephharburg@users.noreply.github.com>
2022-08-31 13:16:25 -0400

    Removed Temporary Access Token request
    It turns out that the endpoint for temporary access tokens is only accessible to already authenticated users within the QuickBase application itself. 
    Thus to my knowledge impossible at the moment to create a temporary access token via an external application.

--- quickbase_rest_api_sdk.php
+++ quickbase_rest_api_sdk.php
@@ -12,12 +12,11 @@
  Class QuickBaseRestApi {
   public $user_token 	= '';	// Valid user token
   public $app_token 	= ''; //Valid app token. Required.
-  private $access_token = ''; //Created with get_and_set_temporary_access_token method
   public $base_url    	= "https://api.quickbase.com/v1"; //The current base url
   public $realm 	= ''; //Quickbase realm string BEFORE .quickbase.com
   public $user_agent 	= ''; //User agent
 
-	public function __construct($user_token='', $app_token = '', $realm = '', $user_agent = '', $access_token = '') {
+  public function __construct($user_token='', $app_token = '', $realm = '', $user_agent = '', $access_token = '') {
     if($user_token) $this->user_token = $user_token;
 
     if($app_token) $this->app_token = $app_token;
@@ -25,57 +24,10 @@
     if($realm) $this->realm = $realm . '.quickbase.com';
 
     if($user_agent) $this->user_token = $user_token;
-
-    if($access_token) $this->access_token = $access_token;
-	}
-
-  /**
-  * Method to set the temporary access token
-  *
-  * @param string $token
-  */
-
-  public function set_access_token($token){
-    $this->access_token = $token;
+		
   }
 
-  /**
-  * Method to get the temporary access token of object instance
-  *
-  */
-
-  public function get_access_token(){
-    return $this->access_token;
-  }
-
-  /**
-  * Method to get and set a temporary access token
-  *
-  * @param string $db_id The id of the database you want access to. Required.
-  */
-
-  public function get_and_set_temporary_access_token($db_id){
-    $headers = array(
-    "QB-Realm-Hostname: $this->realm",
-    "User-Agent: QuickBaseRestApiApp",
-    "QB-App-Token: $this->app_token",
-    "Content-Type: application/json",
-  );
-
-  $url = $this->base_url . "/auth/temporary/$db_id";
-
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
-
-  $response = curl_exec($ch);
-  $token = json_decode($response,true)['temporaryAuthorization'];
-  $this->set_access_token($token);
-  }
-
-  /**
+   /**
   * Method to make the request to QuickBase API
   *
   * @param string $type_of_request The type of http request
@@ -89,7 +41,7 @@
   //See https://developer.quickbase.com/ for actions and endpoints
   private function make_api_request($type_of_request = 'GET', $endpoint = '', $body = ''){
     $url = $this->base_url . $endpoint;
-    $header_token = ($this->get_access_token()) ? "QB-TEMP-TOKEN ". $this->get_access_token() :  "QB-USER-TOKEN " . $this->user_token;
+    $header_token = "QB-USER-TOKEN " . $this->user_token;
     $headers = array(
     "QB-Realm-Hostname: $this->realm",
     "User-Agent: QuickBaseRestApiApp",
